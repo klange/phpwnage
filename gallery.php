17e2982ae3c3e2aac626ec07e530160a6e2389a3
Kevin Lange <klange@dakko.us>
2008-08-22 16:19:55 -0400

    Bit more work on the gallery, just getting things ready to start real work by handling ?do= and POST['action'].

--- gallery.php
+++ gallery.php
@@ -22,41 +22,53 @@
 require 'config.php';
 require 'includes.php';
 
-standardHeaders($site_info['name'],true);
-
-drawSubbar("<a href=\"index.php\">" . $site_info['name'] . "</a> > IMAGE GALLERY","Gallery");
-
-require 'sidebar.php';
-
-print <<<END
-
-<td valign="top">
-<table class="borderless_table" width="100%">
-END;
-$content = "";
-$request = mysql_query("SELECT * FROM `galleries`");
-while ($gal = mysql_fetch_array($request)) {
-    $content = $content . $gal['name'] . "<br /><i>" . $gal['desc'] . "</i><br /><br />";
-}
-$content = $content . <<<END
-<form enctype="multipart/form-data" action="gallery.php" name="form" method="post">
-<input type="hidden" name="MAX_FILE_SIZE" value="200000" />
-<input type="file" name="blam" />
-<input type="submit" value="Upload" />
-</form>
-END;
-if (isset($_FILES['blam'])) {
-    move_uploaded_file($_FILES['blam']['tmp_name'],"/var/www/pwn/upload/" . $_FILES['blam']['name']);
+if (isset($_POST['action'])) {
+    if ($_POST['action'] == "upload") {
+        if (isset($_FILES['image'])) {
+            move_uploaded_file($_FILES['image']['tmp_name'],"/var/www/pwn/upload/" . $_FILES['image']['name']);
+        }
+    }
 }
 
 
-drawBlock("Image Gallery", "Gallery Index", $content);
+if (!$_GET['do'] == "img") {
 
-print <<<END
-	</table>
+    if (!isset($_GET['do']) || ($_GET['do'] == "")) {
+        $content = "";
+        $request = mysql_query("SELECT * FROM `galleries`");
+        while ($gal = mysql_fetch_array($request)) {
+            $content = $content . $gal['name'] . "<br /><i>" . $gal['desc'] . "</i><br /><br />";
+        }
+        $page_contents = makeBlock("Image Gallery", "Gallery Index", $content);
+        $page_location = "Gallery Index";
+        $page_loctitle = " :: Index";
+    } elseif ($_GET['do'] == "upload_form") {
+        $content = <<<END
+        <form enctype="multipart/form-data" action="gallery.php" name="form" method="post">
+            <input type="hidden" name="action" value="upload" />
+            <input type="hidden" name="MAX_FILE_SIZE" value="200000" />
+            <input type="file" name="image" />
+            <input type="submit" value="Upload" />
+        </form>
+END;
+        $page_contents = makeBlock("Image Gallery", "Upload Image", $content);
+        $page_location = "Upload Image";
+        $page_loctitle = " :: Upload Image";
+    }
+
+
+
+    standardHeaders($site_info['name'] . " :: " . "Image Gallery" . $page_loctitle,true);
+    drawSubbar("<a href=\"index.php\">" . $site_info['name'] . "</a> > $page_location","Gallery");
+    require 'sidebar.php';    print <<<END
+        <td valign="top">
+            <table class="borderless_table" width="100%">
+                {$page_contents}
+	        </table>
         </td>
   </tr>
 </table>
 END;
-require 'footer.php';
+    require 'footer.php';
+}
 ?>
