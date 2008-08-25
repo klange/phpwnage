fcd6807b5997496ed9d6501d101977877d8d08d6
Kevin Lange <klange@dakko.us>
2008-08-25 13:17:27 -0400

    Massive amounts of image gallery work.

--- fresh_install.php
+++ fresh_install.php
@@ -570,6 +570,7 @@ CREATE TABLE `galleries` (
     `desc`  text,
     `view`  int(11)         NOT NULL    DEFAULT 0,
     `upload` int(11)        NOT NULL    DEFAULT 1,
+    `thumb` int(11)         NOT NULL    DEFAULT 0,
     PRIMARY KEY `id` (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 END;
@@ -603,7 +604,7 @@ print "Adding generic news item...<br />\n";
 $time = time();
 mysql_query("INSERT INTO `news` VALUES (null, 'Welcome to PHPwnage!', 'Welcome to your new PHPwnage site! Thank you for choosing PHPwnage for your CMS needs. If you have any problems or questions, stop on over at [url=http://oasis-games.com/]our home page[/url]. We are ready to assist anyone who needs help with PHPwnage.', '$time', 'PHPwnage', 0);");
 print "Adding navigation block...<br />\n";
-mysql_query("INSERT INTO `blocks` VALUES (null, 'Navigation', '<a href=\"index.php\">Home</a><br />\n<a href=\"admin.php\">Admin</a><br />\n<a href=\"mobile.php\">Mobile</a><br />\n<a href=\"rss.php\">RSS</a><br />\n<a href=\"forum.php\">Forum</a><br />\n<a href=\"calendar.php\">Calendar</a><br />\n<a href=\"modules.php?m=members\">Member List</a>');");
+mysql_query("INSERT INTO `blocks` VALUES (null, 'Navigation', '<a href=\"index.php\">Home</a><br />\n<a href=\"admin.php\">Admin</a><br />\n<a href=\"mobile.php\">Mobile</a><br />\n<a href=\"rss.php\">RSS</a><br />\n<a href=\"forum.php\">Forum</a><br />\n<a href=\"calendar.php\">Calendar</a><br />\n<a href=\"modules.php?m=members\">Member List</a><br />\n<a href=\"gallery.php\">Image Gallery</a>');");
 print "Completed! Moving to next page...\n";
 print "\n<meta http-equiv=\"Refresh\" content=\"1;url=fresh_install.php?do=page4\">";
 // Now that the core of the installation has completed, grab the $_POST data...
--- gallery.php
+++ gallery.php
@@ -22,38 +22,157 @@
 require 'config.php';
 require 'includes.php';
 
+function generateThumbnail($file, $type) {
+    $_SIZE = 120;
+    switch ($type) {
+    case "image/png":
+        $src = imagecreatefrompng($file);
+        break;
+    case "image/jpeg":
+        $src = imagecreatefromjpeg($file);
+        break;
+    case "image/gif":
+        $src = imagecreatefromgif($file);
+        break;
+    default:
+        return false;
+    }
+    $im = imagecreatetruecolor($_SIZE,$_SIZE);
+    imagesavealpha($im, true);
+    $trans = imagecolorallocatealpha($im, 0, 0, 0, 127);
+    imagefill($im, 0, 0, $trans);
+    $wid = imagesx($src);
+    $hei = imagesy($src);
+    if ($wid < $_SIZE && $hei < $_SIZE) {
+        imagesavealpha($src, true);
+        imagepng($src, $file . "_th");
+    } else {
+        $ratio = $wid / $hei;
+        if ($ratio > 1) {
+            $newsize = $_SIZE / $ratio;
+            $offset = ($_SIZE - $newsize) / 2;
+            imagecopyresampled($im, $src, 0, $offset, 0, 0, $_SIZE, $_SIZE / $ratio, $wid, $hei);
+        } else {
+            $newsize = $_SIZE * $ratio;
+            $offset = ($_SIZE - $newsize) / 2;
+            imagecopyresampled($im, $src, $offset, 0, 0, 0, $_SIZE * $ratio, $_SIZE, $wid, $hei);
+        }
+        imagesavealpha($im, true);
+        imagepng($im, $file . "_th");
+    }
+    imagedestroy($im);
+    return true;
+}
+
 if (isset($_POST['action'])) {
     if ($_POST['action'] == "upload") {
         if (isset($_FILES['image'])) {
-            move_uploaded_file($_FILES['image']['tmp_name'],"/var/www/pwn/upload/" . $_FILES['image']['name']);
+            if (!generateThumbnail($_FILES['image']['tmp_name'],$_FILES['image']['type'])) {
+                messageBack("Image Gallery","Upload failed. File may be too large or of an unknown type.");    
+            }
+            $fname = $_FILES['image']['tmp_name'];
+            $file = fopen($fname,"rb");
+            $image = addslashes(fread($file,$_FILES['image']['size']));
+            $fname = $_FILES['image']['tmp_name'] . "_th";
+            $file = fopen($fname,"rb");
+            $thumb = addslashes(fread($file,filesize($fname)));
+            $query = <<<END
+INSERT INTO `images` VALUES (
+NULL, '{$_POST['name']}', '{$_POST['desc']}', {$user['id']},   
+'{$_FILES['image']['name']}', {$_POST['gallery']}, {$_FILES['image']['size']},
+'{$_FILES['image']['type']}', 1, "{$image}", "{$thumb}");
+END;
+            mysql_query($query);
+            $result = mysql_query("SELECT `id` FROM `images` ORDER BY `id` DESC LIMIT 1");
+            $newimage = mysql_fetch_array($result);
+            unlink($_FILES['image']['tmp_name']);
+            unlink($_FILES['image']['tmp_name'] . "_th");
+            messageRedirect("Image Gallery","Image uploaded!","gallery.php?do=image&amp;id={$newimage['id']}");
+        } else {
+            messageBack("Image Gallery","No image specified");
         }
     }
 }
 
-
-if (!$_GET['do'] == "img") {
+if ($_GET['do'] != "img") {
 
     if (!isset($_GET['do']) || ($_GET['do'] == "")) {
-        $content = "";
+        $content = "<table class=\"forum_base\" width=\"100%\">";
         $request = mysql_query("SELECT * FROM `galleries`");
         while ($gal = mysql_fetch_array($request)) {
-            $content = $content . $gal['name'] . "<br /><i>" . $gal['desc'] . "</i><br /><br />";
+            if ($gal['thumb'] != 0) {
+                $gal_thumb = "<img src=\"gallery.php?do=img&amp;type=thumb&amp;i={$gal['thumb']}\" alt=\"\" />";
+            } else {
+                $gal_thumb = "<img src=\"tango/admin/images.png\" alt=\"\" />";
+            }
+            $content = $content . "<tr><td width=\"1\" class=\"forum_topic_content\" align=\"center\" valign=\"middle\">{$gal_thumb}</td><td class=\"forum_topic_content\"><a href=\"gallery.php?do=view&amp;id={$gal['id']}\">{$gal['name']}</a><br /><i>{$gal['desc']}</i></td></tr>\n";
         }
+        $content = $content . "</table>";
         $page_contents = makeBlock("Image Gallery", "Gallery Index", $content);
-        $page_location = "Gallery Index";
+        $page_location = "<a href=\"gallery.php\">Image Gallery</a>";
         $page_loctitle = " :: Index";
     } elseif ($_GET['do'] == "upload_form") {
+        $poster = printPoster('desc');
         $content = <<<END
         <form enctype="multipart/form-data" action="gallery.php" name="form" method="post">
             <input type="hidden" name="action" value="upload" />
-            <input type="hidden" name="MAX_FILE_SIZE" value="200000" />
-            <input type="file" name="image" />
-            <input type="submit" value="Upload" />
+            <input type="hidden" name="gallery" value="{$_GET['gal']}" />
+            <input type="hidden" name="MAX_FILE_SIZE" value="20000000" />
+            <table class="forum_base" width="100%">
+                <tr><td class="forum_topic_content" width="200">Name</td><td class="forum_topic_content"><input type="text" name="name" style="width: 100%" /></td></tr>
+                <tr><td class="forum_topic_sig" colspan="2">{$poster}<textarea name="desc" style="width: 100%" rows="5" cols="80"></textarea></td></tr>
+                <tr><td class="forum_topic_sig">Image</td><td class="forum_topic_sig"><input type="file" name="image" /></td></tr>
+                <tr><td class="forum_topic_sig" colspan="2"><input type="submit" value="Upload" /></td></tr>
+            </table>
         </form>
 END;
         $page_contents = makeBlock("Image Gallery", "Upload Image", $content);
-        $page_location = "Upload Image";
+        $page_location = "<a href=\"gallery.php\">Image Gallery</a> > Upload Image";
         $page_loctitle = " :: Upload Image";
+    } elseif ($_GET['do'] == "view") {
+        $request = mysql_query("SELECT * FROM `galleries` WHERE `id`={$_GET['id']}");
+        $gal = mysql_fetch_array($request);
+        $content = "<table class=\"mod_set\">";
+        if ($user['level'] >= $gal['upload']) {
+            $content = $content . drawButton("gallery.php?do=upload_form&amp;gal={$gal['id']}","Upload");
+        }
+        $content = $content . "</table>";
+        $content = $content . "<table class=\"forum_base\" width=\"100%\">";
+        $request = mysql_query("SELECT `id`,`name`,`desc`,`uid`,`fname`,`publ` FROM `images` WHERE `gid`={$gal['id']} ORDER BY `id` DESC");
+        while ($image = mysql_fetch_array($request)) {
+            $content = $content . "<tr><td class=\"forum_topic_content\" width=\"1\" rowspan=\"2\" align=\"center\" valign=\"middle\"><img src=\"gallery.php?do=img&amp;type=thumb&amp;i={$image['id']}\" alt=\"\" /></td>";
+            $content = $content . "<td class=\"forum_topic_content\"><b><a href=\"gallery.php?do=image&amp;id={$image['id']}\">{$image['name']}</a></b></td>";
+            $results = mysql_query("SELECT `name` FROM `users` WHERE `id`={$image['uid']}");
+            $uploader = mysql_fetch_array($results);
+            $content = $content . "<td class=\"forum_topic_content\" width=\"200\">Uploaded by <a href=\"forum.php?do=viewprofile&amp;id={$image['uid']}\">{$uploader['name']}</a></td>";
+            $description = bbDecode($image['desc']);
+            $content = $content . "</tr><tr><td class=\"forum_topic_sig\" colspan=\"2\">{$description}</td></tr>";
+        }
+        $content = $content . "</table>";
+        
+        
+        $page_contents = makeBlock("Image Gallery","Viewing Gallery", $content);
+        $page_location = "<a href=\"gallery.php\">Image Gallery</a> > " . $gal['name'];
+        $page_loctitle = " :: " . $gal['name'];
+    } elseif ($_GET['do'] == "image") {
+        $request = mysql_query("SELECT `name`,`desc`,`uid`,`fname`,`publ`,`gid` FROM `images` WHERE `id`={$_GET['id']}");
+        $image = mysql_fetch_array($request);
+        $results = mysql_query("SELECT `id`, `name` FROM `users` WHERE `id`={$image['uid']}");
+        $uploader = mysql_fetch_array($results);
+        $results = mysql_query("SELECT * FROM `galleries` WHERE `id`={$image['gid']}");
+        $gal = mysql_fetch_array($results);
+        $desc = bbDecode($image['desc']);
+        $content = <<<END
+<table class="forum_base" width="100%">        
+<tr><td class="forum_topic_content" align="center"><b>{$image['name']}</b></td></tr>
+<tr><td class="forum_topic_sig" align="center">Uploaded by <a href="forum.php?do=viewprofile&amp;id={$uploader['id']}">{$uploader['name']}</a></td></tr>
+<tr><td class="forum_topic_sig" align="center"><img src="gallery.php?do=img&amp;i={$_GET['id']}" alt="{$image['name']}" /></tr></td>
+<tr><td class="forum_topic_sig" align="center">{$desc}</td></tr>
+</table>
+END;
+        $page_contents = makeBlock("Image Gallery",$image['name'], $content);
+        $page_location = "<a href=\"gallery.php\">Image Gallery</a> > <a href=\"gallery.php?do=view&amp;id={$gal['id']}\">" . $gal['name'] . "</a> > " . $image['name'];
+        $page_loctitle = " :: " . $gal['name'] . " :: " . $image['name'];
     }
 
 
@@ -70,5 +189,18 @@ END;
 </table>
 END;
     require 'footer.php';
+} else {
+    // We're procesing image requests here.
+    if (!isset($_GET['type']) || $_GET['type'] == "img") {
+        $results = mysql_query("SELECT `type`, `data` FROM `images` WHERE `id`={$_GET['i']}");
+        $image = mysql_fetch_array($results);
+        header("Content-type: " . $image['type']);
+        die($image['data']);
+    } elseif ($_GET['type'] == "thumb") {
+        $results = mysql_query("SELECT `thumb` FROM `images` WHERE `id`={$_GET['i']}");
+        $image = mysql_fetch_array($results);
+        header("Content-type: image/png");
+        die($image['thumb']);
+    }
 }
 ?>
